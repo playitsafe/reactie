2c149b56f8fb85aab243bb31eb7484afd4c5d1ce
Aaron <playitsafe@qq.com>
2025-06-15 20:20:33 +0800

    style: change code style config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,51 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import json from "@eslint/json";
-import prettierPlugin from "eslint-plugin-prettier";
-import * as prettierConfig from "eslint-plugin-prettier/recommended";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import json from "@eslint/json"
+import prettierPlugin from "eslint-plugin-prettier"
+import * as prettierConfig from "eslint-plugin-prettier/recommended"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  // JS/TS base config
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest,
-      },
-      parser: tseslint.parser,
-    },
-    plugins: {
-      js,
-      "@typescript-eslint": tseslint.plugin,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      "prettier/prettier": "error",
-      "no-case-declarations": "off",
-      "no-constant-condition": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "no-unused-vars": "off",
-    },
-  },
+	// JS/TS base config
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.jest
+			},
+			parser: tseslint.parser
+		},
+		plugins: {
+			js,
+			"@typescript-eslint": tseslint.plugin,
+			prettier: prettierPlugin
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...tseslint.configs.recommended.rules,
+			"prettier/prettier": "error",
+			"no-case-declarations": "off",
+			"no-constant-condition": "off",
+			"@typescript-eslint/ban-ts-comment": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/no-var-requires": "off",
+			"no-unused-vars": "off"
+		}
+	},
 
-  // Prettier recommended config
-  prettierConfig.default,
+	// Prettier recommended config
+	prettierConfig.default,
 
-  // JSON files
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json",
-    extends: ["json/recommended"],
-  },
-]);
+	// JSON files
+	{
+		files: ["**/*.json"],
+		plugins: { json },
+		language: "json",
+		extends: ["json/recommended"]
+	}
+])
